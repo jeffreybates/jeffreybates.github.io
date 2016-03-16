33c32151de6be1b35cb0579d01ada2684a57c880
jeffreybates <batmanbates@me.com>
2016-03-15 17:26:50 -0700

    basic menu thang

---
+++ js/menu.js
@@ -0,0 +1,113 @@
+
+// list item one
+document.getElementById("1st").onmouseover = function() {overone()};
+document.getElementById("1st").onmouseout = function() {outone()};
+
+function overone() {
+	document.getElementById("1st").style.fontSize = "40pt";
+
+}
+
+function outone() {
+	document.getElementById("1st").style.fontSize = "20pt";
+}
+
+// list item two
+document.getElementById("2nd").onmouseover = function() {overtwo()};
+document.getElementById("2nd").onmouseout = function() {outtwo()};
+
+function overtwo() {
+	document.getElementById("2nd").style.fontSize = "40pt";
+
+}
+
+function outtwo() {
+	document.getElementById("2nd").style.fontSize = "20pt";
+}
+
+// list item three
+document.getElementById("3rd").onmouseover = function() {overthree()};
+document.getElementById("3rd").onmouseout = function() {outthree()};
+
+
+function overthree() {
+	document.getElementById("3rd").style.fontSize = "40pt";
+
+}
+
+function outthree() {
+	document.getElementById("3rd").style.fontSize = "20pt";
+}
+
+// list item four
+document.getElementById("4th").onmouseover = function() {overfour()};
+document.getElementById("4th").onmouseout = function() {outfour()};
+
+
+function overfour() {
+	document.getElementById("4th").style.fontSize = "40pt";
+
+}
+
+function outfour() {
+	document.getElementById("4th").style.fontSize = "20pt";
+}
+
+// list item five
+document.getElementById("5th").onmouseover = function() {overfive()};
+document.getElementById("5th").onmouseout = function() {outfive()};
+
+
+function overfive() {
+	document.getElementById("5th").style.fontSize = "40pt";
+
+}
+
+function outfive() {
+	document.getElementById("5th").style.fontSize = "20pt";
+}
+
+// list item six
+document.getElementById("6th").onmouseover = function() {oversix()};
+document.getElementById("6th").onmouseout = function() {outsix()};
+
+
+function oversix() {
+	document.getElementById("6th").style.fontSize = "40pt";
+
+}
+
+function outsix() {
+	document.getElementById("6th").style.fontSize = "20pt";
+}
+
+// list item seven
+document.getElementById("7th").onmouseover = function() {overseven()};
+document.getElementById("7th").onmouseout = function() {outseven()};
+
+
+function overseven() {
+	document.getElementById("7th").style.fontSize = "40pt";
+
+}
+
+function outseven() {
+	document.getElementById("7th").style.fontSize = "20pt";
+}
+
+// list item eight
+document.getElementById("8th").onmouseover = function() {overeight()};
+document.getElementById("8th").onmouseout = function() {outeight()};
+
+
+function overeight() {
+	document.getElementById("8th").style.fontSize = "40pt";
+
+}
+
+function outeight() {
+	document.getElementById("8th").style.fontSize = "20pt";
+}
+
+
+
