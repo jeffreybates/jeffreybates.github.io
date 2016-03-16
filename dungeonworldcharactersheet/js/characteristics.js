a5ce960662ad32871fbc3c96443ca973ece56eae
jeffreybates <batmanbates@me.com>
2016-03-15 21:15:15 -0700

    adding dungeon world character sheet

---
+++ dungeonworldcharactersheet/js/characteristics.js
@@ -0,0 +1,218 @@
+
+
+
+function calcStrength() {
+
+var STRoutput;
+var strset = document.getElementById("strset").value;
+var yesworkie = "yes";
+var noworkie = "no";
+
+if (strset >= 1 && strset <= 3) {
+    document.getElementById("STRoutput").innerHTML = -3;
+}  { if (strset >= 4 && strset <= 5)
+   document.getElementById("STRoutput").innerHTML = -2;
+}
+ { if (strset >= 6 && strset <= 8)
+   document.getElementById("STRoutput").innerHTML = -1;
+
+ { if (strset >= 9 && strset <= 12)
+   document.getElementById("STRoutput").innerHTML = 0;
+
+	
+ {  if (strset >= 13 && strset <= 15)
+   document.getElementById("STRoutput").innerHTML = 1;
+	
+ { if (strset >= 16 && strset <= 17)
+   document.getElementById("STRoutput").innerHTML = 2;
+	
+ { if (strset == 18)
+   document.getElementById("STRoutput").innerHTML = 3;
+}
+}
+}
+}
+}
+}
+
+
+function calcDexterity() {
+
+var DEXoutput;
+var dexset = document.getElementById("dexset").value;
+var yesworkie = "yes";
+var noworkie = "no";
+
+if (dexset >= 1 && dexset <= 3) {
+    document.getElementById("DEXoutput").innerHTML = -3;
+}  { if (dexset >= 4 && dexset <= 5)
+   document.getElementById("DEXoutput").innerHTML = -2;
+}
+ { if (dexset >= 6 && dexset <= 8)
+   document.getElementById("DEXoutput").innerHTML = -1;
+
+ { if (dexset >= 9 && dexset <= 12)
+   document.getElementById("DEXoutput").innerHTML = 0;
+
+	
+ {  if (dexset >= 13 && dexset <= 15)
+   document.getElementById("DEXoutput").innerHTML = 1;
+	
+ { if (dexset >= 16 && dexset <= 17)
+   document.getElementById("DEXoutput").innerHTML = 2;
+	
+ { if (dexset == 18)
+   document.getElementById("DEXoutput").innerHTML = 3;
+}
+}
+}
+}
+}
+}
+
+
+function calcConstitution() {
+
+var CONoutput;
+var conset = document.getElementById("conset").value;
+var secretvalue = 15;
+var yesworkie = "yes";
+var noworkie = "no";
+
+if (conset >= 1 && conset <= 3) {
+    document.getElementById("CONoutput").innerHTML = -3;
+}  { if (conset >= 4 && conset <= 5)
+   document.getElementById("CONoutput").innerHTML = -2;
+}
+ { if (conset >= 6 && conset <= 8)
+   document.getElementById("CONoutput").innerHTML = -1;
+
+ { if (conset >= 9 && conset <= 12)
+   document.getElementById("CONoutput").innerHTML = 0;
+
+	
+ {  if (conset >= 13 && conset <= 15)
+   document.getElementById("CONoutput").innerHTML = 1;
+	
+ { if (conset >= 16 && conset <= 17)
+   document.getElementById("CONoutput").innerHTML = 2;
+	
+ { if (conset == 18)
+   document.getElementById("CONoutput").innerHTML = 3;
+}
+}
+}
+}
+}
+}
+
+
+function calcIntelligence() {
+
+var INToutput;
+var intset = document.getElementById("intset").value;
+var secretvalue = 15;
+var yesworkie = "yes";
+var noworkie = "no";
+
+if (intset >= 1 && intset <= 3) {
+    document.getElementById("INToutput").innerHTML = -3;
+}  { if (intset >= 4 && intset <= 5)
+   document.getElementById("INToutput").innerHTML = -2;
+}
+ { if (intset >= 6 && intset <= 8)
+   document.getElementById("INToutput").innerHTML = -1;
+
+ { if (intset >= 9 && intset <= 12)
+   document.getElementById("INToutput").innerHTML = 0;
+
+	
+ {  if (intset >= 13 && intset <= 15)
+   document.getElementById("INToutput").innerHTML = 1;
+	
+ { if (intset >= 16 && intset <= 17)
+   document.getElementById("INToutput").innerHTML = 2;
+	
+ { if (intset == 18)
+   document.getElementById("INToutput").innerHTML = 3;
+}
+}
+}
+}
+}
+}
+
+
+function calcWisdom() {
+
+var WISoutput;
+var wisset = document.getElementById("wisset").value;
+var secretvalue = 15;
+var yesworkie = "yes";
+var noworkie = "no";
+
+if (wisset >= 1 && wisset <= 3) {
+    document.getElementById("WISoutput").innerHTML = -3;
+}  { if (wisset >= 4 && wisset <= 5)
+   document.getElementById("WISoutput").innerHTML = -2;
+}
+ { if (wisset >= 6 && wisset <= 8)
+   document.getElementById("WISoutput").innerHTML = -1;
+
+ { if (wisset >= 9 && wisset <= 12)
+   document.getElementById("WISoutput").innerHTML = 0;
+
+	
+ {  if (wisset >= 13 && wisset <= 15)
+   document.getElementById("WISoutput").innerHTML = 1;
+	
+ { if (wisset >= 16 && wisset <= 17)
+   document.getElementById("WISoutput").innerHTML = 2;
+	
+ { if (wisset == 18)
+   document.getElementById("WISoutput").innerHTML = 3;
+}
+}
+}
+}
+}
+}
+
+function calcCharisma() {
+
+var CHAoutput;
+var chaset = document.getElementById("chaset").value;
+var secretvalue = 15;
+var yesworkie = "yes";
+var noworkie = "no";
+
+if (chaset >= 1 && chaset <= 3) {
+    document.getElementById("CHAoutput").innerHTML = -3;
+}  { if (chaset >= 4 && chaset <= 5)
+   document.getElementById("CHAoutput").innerHTML = -2;
+}
+ { if (chaset >= 6 && chaset <= 8)
+   document.getElementById("CHAoutput").innerHTML = -1;
+
+ { if (chaset >= 9 && chaset <= 12)
+   document.getElementById("CHAoutput").innerHTML = 0;
+
+	
+ {  if (chaset >= 13 && chaset <= 15)
+   document.getElementById("CHAoutput").innerHTML = 1;
+	
+ { if (chaset >= 16 && chaset <= 17)
+   document.getElementById("CHAoutput").innerHTML = 2;
+	
+ { if (chaset == 18)
+   document.getElementById("CHAoutput").innerHTML = 3;
+}
+}
+}
+}
+}
+}
+
+
+
+
---
+++ dungeonworldcharactersheet/js/hitpoints.js
@@ -0,0 +1,9 @@
+
+
+function myHP (){
+var baseHP =  6;
+var conset =  parseInt(document.getElementById("conset").value);
+document.getElementById("hitpointvalue").innerHTML = (conset + baseHP);
+
+}
+
