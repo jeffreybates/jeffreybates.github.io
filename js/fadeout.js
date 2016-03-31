32d9a0d66c4fd72f4ba92d5d19cc9a5e2e2951bd
jeffreybates <batmanbates@me.com>
2016-03-31 14:45:30 -0700

    fade in / out
    saving this just in case

--- js/fadeout.js
+++ js/fadeout.js
@@ -1,4 +1,29 @@
+
+$( "#phaseOne" ).hide(function(){
+});
+
+
+// concierge phase one
+
 $( "#content1" ).click(function() {
+
+  $( "#phaseOne" ).fadeIn( 1500, function() {
+    // Animation complete.
+  });
+
+  $( "#phaseOne" ).fadeOut( 1500, function() {
+    // Animation complete.
+  });
+});
+
+// tetra phase one
+
+$( "#1st" ).click(function() {
+
+  $( "#phaseOne" ).fadeIn( 1500, function() {
+    // Animation complete.
+  });
+
   $( "#phaseOne" ).fadeOut( 1500, function() {
     // Animation complete.
   });
